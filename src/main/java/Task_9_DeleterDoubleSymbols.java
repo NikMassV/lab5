50f1d5c4dd3e66f5bee74516fcbfa8793a3d0a3d
NikMassV <56344046+NikMassV@users.noreply.github.com>
2020-11-04 02:10:02 +0300

    complited hometasks

--- src/main/java/Task_9_DeleterDoubleSymbols.java
+++ src/main/java/Task_9_DeleterDoubleSymbols.java
@@ -19,36 +19,3 @@ public class Task_9_DeleterDoubleSymbols {
         System.out.println("Took time: " + (endTime - startTime) / 1000d + " sec.");
     }
 }
-
-//    long startTime = System.currentTimeMillis();
-//        System.out.println("---Task 9---");
-//                List<String> list = FileReader.readFile(fileName);
-//        List<String> resultList = new LinkedList<>();
-//        for (String s : list) {
-//        char[] string = s.toCharArray();
-//        if (string.length > 0) {
-//        int counter = 0;
-//        for (int i = 1; i < string.length - counter; i++) {
-//        if (i == (string.length - 1) && string[string.length - 1] == string[string.length - 2]) {
-//        counter++;
-//        break;
-//        } else if (string[i] == string[i - 1]) {
-//        for (int j = i; j <= string.length - 1 - counter; j++) {
-//        if (j == string.length - 1 - counter) {
-//        counter++;
-//        break;
-//        } else {
-//        string[j - 1] = string[j];
-//        }
-//        }
-//        }
-//        }
-//        String resultString = String.copyValueOf(string, 0, string.length - 1 - counter);
-//        resultList.add(resultString);
-//        }
-//        }
-//        for (String s : resultList) {
-//        System.out.println(s);
-//        }
-//        long endTime = System.currentTimeMillis();
-//        System.out.println("Took time: " + (endTime - startTime) / 1000d + " sec.");
